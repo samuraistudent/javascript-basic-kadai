063364f5a208703713ffcb9e5d9a3d901dcbb267
samuraistudent <trickers0826@gmail.com>
2025-04-19 08:17:51 +0900

    Add files via upload

---
+++ kadai_020/event.js
@@ -0,0 +1,6 @@
+const clickBtn = document.getElementById('btn');
+
+clickBtn.addEventListener('click', ( )=> {
+    const textBtn = document.getElementById('text');
+    console.log('ボタンをクリックしました');
+})

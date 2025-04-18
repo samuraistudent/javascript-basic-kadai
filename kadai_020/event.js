8903b6c0c786f6867301b8de5168ad562947aa91
samuraistudent <trickers0826@gmail.com>
2025-04-19 08:36:01 +0900

    Delete kadai_020 directory

--- kadai_020/event.js
+++
@@ -1,6 +0,0 @@
-const clickBtn = document.getElementById('btn');
-
-clickBtn.addEventListener('click', ( )=> {
-    const textBtn = document.getElementById('text');
-    console.log('ボタンをクリックしました');
-})
